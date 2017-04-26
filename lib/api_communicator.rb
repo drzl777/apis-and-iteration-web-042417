f50b5532d28f606e2076832c0499ec84b3f04262
drzly777 <andrew.zheng725@gmail.com>
2017-04-26 14:17:45 -0400

    created startwars api methods

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,11 +2,29 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
+#binding.pry
+def get_all_characters
+  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  all_character_array = JSON.parse(all_characters)["results"]
+end
+
+def get_character_films(character)
+  all_character_array = get_all_characters
+  this_character_hash = all_character_array.find {|character_hash| character_hash["name"].downcase == character}
+  film_array = this_character_hash["films"]
+end
+
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
+
+  film_array = get_character_films(character)
+  film_info_array = film_array.collect {|film_url| JSON.parse(RestClient.get(film_url))}
+
+
+
+
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -20,6 +38,7 @@ end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each {|film| puts film["title"]}
 end
 
 def show_character_movies(character)
